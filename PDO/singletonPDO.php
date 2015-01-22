f540d376a2405cd345f1570e8171108bf02fb70d
zhentaoo <1904974327@qq.com>
2015-01-22 09:48:54 +0800

    pdo单例模式

--- PDO/singletonPDO.php
+++ PDO/singletonPDO.php
@@ -1,100 +1,71 @@
 <?php
 
-/*
-* mysql 单例
-*/
-
-class mysql
+class SingletonPDO
 {
-    private $host = 'localhost'; //数据库主机
-    private $user = 'root'; //数据库用户名
-    private $pwd = '123456'; //数据库用户名密码
-    private $database = 'monster'; //数据库名
-    private $charset = 'utf8'; //数据库编码，GBK,UTF8,gb2312
-    private $link;             //数据库连接标识;
-    private $rows;             //查询获取的多行数组
-    static $_instance; //存储对象
+    private $dbms;//数据库类型
+    private $host;//数据库主机名
+    private $dbName;//使用的数据库
+    private $user;//数据库连接用户名
+    private $pass;//对应的密码
+    private $dsn;
+    private $pdo;
+    private static $_instance; //存储对象
 
-    /**
-     * 构造函数
-     * 私有
-     */
-    private function __construct($pconnect = false)
+    private function __construct()
     {
-        if (!$pconnect) {
-            $this->link = @ mysql_connect($this->host, $this->user, $this->pwd) or $this->err();
-        } else {
-            $this->link = @ mysql_pconnect($this->host, $this->user, $this->pwd) or $this->err();
-        }
-        mysql_select_db($this->database) or $this->err();
-        $this->query("SET NAMES '{$this->charset}'", $this->link);
-        return $this->link;
+        $this->dbms = 'mysql';     //数据库类型
+        $this->host = 'localhost'; //数据库主机名
+        $this->dbName = 'monster';    //使用的数据库
+        $this->user = 'root';      //数据库连接用户名
+        $this->pass = '123456';          //对应的密码
+        $this->dsn = "$this->dbms:host=$this->host;dbname=$this->dbName";
+        $this->pdo = new PDO($this->dsn, $this->user, $this->pass, array(PDO::ATTR_PERSISTENT => true));
+        $this->pdo->query('set names uft-8');
     }
 
-    /**
-     * 防止被克隆
-     *
-     */
     private function __clone()
     {
     }
 
-    public static function getInstance($pconnect = false)
+    public static function getInstance()
     {
-        if (FALSE == (self::$_instance instanceof self)) {
-            self::$_instance = new self($pconnect);
+        if (self::$_instance == null) {
+//            self::$_instance = new SingletonPDO();
+            self::$_instance = new self();
         }
         return self::$_instance;
     }
 
-    /**
-     * 查询
-     */
-    public function query($sql, $link = '')
+    public function query($sql)
     {
-        $this->result = mysql_query($sql, $this->link) or $this->err($sql);
-        return $this->result;
-    }
+        $rows = $this->pdo->query($sql);
+        $result = array();
 
-    /**
-     * 单行记录
-     */
-    public function getRow($sql, $type = MYSQL_ASSOC)
-    {
-        $result = $this->query($sql);
-        return @ mysql_fetch_array($result, $type);
-    }
-
-    /**
-     * 多行记录
-     */
-    public function getRows($sql, $type = MYSQL_ASSOC)
-    {
-        $result = $this->query($sql);
-        while ($row = @ mysql_fetch_array($result, $type)) {
-            $this->rows[] = $row;
+        $i = 0;
+        foreach ($rows as $row) {
+            foreach ($row as $k => $v) {
+                if (!is_numeric($k))
+                    $result[$i][$k] = $v;
+            }
+            $i++;
         }
-        return $this->rows;
+        return $result;
     }
 
-    /**
-     * 错误信息输出
-     */
-    protected function err($sql = null)
+    public function update($sql)
     {
-        //这里输出错误信息
-        echo 'error';
-        exit();
+        $this->pdo->query($sql);
     }
 }
 
-//用例
-$db = mysql::getInstance();
-$db2 = mysql::getInstance();
-$data = $db->getRows('select * from user');
-print_r($data);
-//判断两个对象是否相等
-if ($db === $db2) {
-    echo 'true';
+$test = SingletonPDO::getInstance();
+//var_dump($test->query("select * from user where name='test'"));
+var_dump($test->query("select * from user"));
+
+$test2 = SingletonPDO::getInstance();
+
+if ($test == $test2) {
+    echo 'ok';
+} else {
+    echo 'hehe';
 }
-?>
