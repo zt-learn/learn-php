46805237256ca77e6e9b1534cb57953183e0b1f3
zhentaoo <1904974327@qq.com>
2015-01-30 10:50:42 +0800

    array测试

--- Array/ArrayTest.php
+++ Array/ArrayTest.php
@@ -4,63 +4,28 @@ $array = array(
     'b' => array('a' => '4', 'b' => '5', 'c' => '6'),
     'c' => array('a' => '7', 'b' => '8', 'c' => '9')
 );
-echo sizeof($array);
 
-$test = array();
-foreach ($array as $key => $value) {
-//    echo $key;
-//    var_dump($value);
-    $test[$key] = $value;
-}
-var_dump($test);
-
-$array2 = $array;
-echo '*********<hr>******';
-print_r($array2);
-echo '<br>';
-$array2['a'] = ['a' => 'ss', 'b' => 'bb', 'c' => 'cc'];
-print_r($array2);
-echo '<br>';
-print_r($array);
-echo '<br>';
-
-
-echo '<hr>';
+$testArray1 = array();
 foreach ($array as $k => $v) {
-    echo '<br>' . $k . '<br>';
-    foreach ($v as $data)
-        echo $data . ' ';
+    $testArray1[$k] = $v;
 }
+
+var_dump($array);
+var_dump($testArray1);
+
 echo '<hr>';
 
-/*
- * sort排序，两种方式：SORT_NUMERIC，SORT_STRINGs；
- * asort：根据值排序
- * ksort：根据键排序
- */
-
-print_r($array['a']);
-echo '<br>';
-asort($array['a'], SORT_NUMERIC);
-print_r($array['a']);
-echo '<br>';
+$array2 = array(
+    array('a' => '1212', 'b' => '2', 'c' => '3'),
+    array('a' => '4', 'b' => '5', 'c' => '6'),
+    array('a' => '7', 'b' => '8', 'c' => '9')
+);
 
 
-//foreach ($array as $k => $v) {
-//    echo '<br>' . $k . '<br>';
-//    foreach ($v as $data)
-//        echo $data . ' ';
-//}
-//echo '<hr>';
-//$new = [
-//    'a' => ['a' => '1', 'b' => '2', 'c' => '3'],
-//    ['a' => '4', 'b' => '5', 'c' => '6'],
-//    ['a' => '7', 'b' => '8', 'c' => '9'],
-//];
-//
-//foreach ($new as $k => $v) {
-//    echo '<br>' . $k . '<br>';
-//    foreach ($v as $data) {
-//        echo $data;
-//    }
-//}
+$testArray2 = array();
+for ($i = 0; $i < sizeof($array2); $i++) {
+    $testArray2[$i] = $array2[$i];
+}
+
+var_dump($array2);
+var_dump($testArray2);
