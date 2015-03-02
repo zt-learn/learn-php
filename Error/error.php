53d5cba2f5b1bbed1f51efb9f291fbfb572a4bf0
zhentaoo <1904974327@qq.com>
2015-03-02 10:16:39 +0800

    错误测试函数，获取当前页面url函数

---
+++ Error/error.php
@@ -0,0 +1,5 @@
+<?php
+
+if (1) {
+    trigger_error("zhe shi cuo wu xin xi ", E_USER_ERROR);
+}
--- str/strTest.php
+++ str/strTest.php
@@ -1,6 +1,14 @@
 <?php
-echo strtolower("sdf_SD_sdf");
-echo '<hr/>';
-echo strtr("sdf_SD_sdf", "_", "");
-echo '<br>';
-echo str_replace("_", "", "sdf_SD_sdf");
+echo strtolower("sdf_SD_sdf") . '<hr>';
+
+//字符串替换，但是不能替换成空
+echo strtr("sdf_SD_sdf", "_", "") . '<hr>';
+
+//字符串替换
+echo str_replace("_", "", "sdf_SD_sdf") . '<hr>';
+
+
+echo ucfirst('asdf') . '<hr>';
+
+//确定是不是数字，或数字字符串
+echo is_numeric('123123') . '<hr>';
