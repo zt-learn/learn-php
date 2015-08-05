e01c3974e705cd23aad453176a8a6db7f43e221a
zhentaoo <1904974327@qq.com>
2015-08-05 13:11:03 +0800

    php正则表达式学习

--- RegExp/index.php
+++ RegExp/index.php
@@ -16,12 +16,11 @@
 //if (preg_match($regex, $str, $matches)) {
 //    var_dump($matches);
 //}
-//
 
-$str = '“欢迎查看美女图片<img src="/ueditor/php/upload/image/20150730/1438267857365156.png" width="450" height="210" />哈哈！<img src="images/new/h1.jpg" width="450" height="210" />”';
+$str = '&lt;p&gt;&lt;img src="/ueditor/php/upload/image/20150730/1438266463281325.jpg" title="1438266463281325.jpg" alt="6.jpg"&gt;&lt;/p&gt;';
 //echo $str;
-preg_match('/ueditor.*(png|gif|jpg)/U', $str, $out);
-var_dump($out);
+preg_match('/\/ueditor.*(png|gif|jpg)/U', $str, $out);
+var_dump($out[0]);
 
 //$img = $out[0][0];
 //echo $img;
